13712817180aff86685776dd40b0436f03df6f76
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-03 14:40:42 -0400

    i think this changed the gitignore

--- setup/fetch.py
+++ setup/fetch.py
@@ -5,9 +5,7 @@ import subprocess
 import os
 
 def setup_dirs():
-    subprocess.run('mkdir ../vendor/')
     subprocess.run('mkdir ../lib/')
-    subprocess.run('mkdir ../include/')
     subprocess.run('mkdir ../include/GLFW')
 
 
@@ -18,7 +16,7 @@ def install_glfw():
     request = requests.get('https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.bin.WIN64.zip')
     glfw_zfile = ZipFile(io.BytesIO(request.content))
     glfw_zfile.extract(os.path.join(glfw_version, '/lib-vc2019/glfw3.lib'), '../lib/')
-    glfw_zfile.extract(os.path.join(glfw_version, 'glfw-3.3.2.bin.WIN64/include/GLFW/glfw3.h'), 
+    glfw_zfile.extract(os.path.join(glfw_version, '/include/GLFW/glfw3.h'), 
                                                 '../include/GLFW/')
     glfw_zfile.close()
 
