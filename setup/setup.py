d252368b43a0ccd5f7692c4318673d2851a7ac31
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-03 23:37:13 -0400

    added premake command to setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -4,4 +4,4 @@ subprocess.call('pip install requirements.txt')
 import fetch
 fetch.install_glfw()
 
-subprocess.call('vendor/bin/premake5 premake5.lua')
+subprocess.call('../vendor/bin/premake5 vs2019')
