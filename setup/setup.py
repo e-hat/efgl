bbbe0dbaf678d9fc6cd5c8ee56f42ceea19cd2f4
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-06 11:00:48 -0400

    fixed tiny bug

--- setup/setup.py
+++ setup/setup.py
@@ -2,5 +2,6 @@
 having a python file for this is a little heavy duty,
 I'm thinking of only doing a batch file instead
 """
+import subprocess
 # maybe make this an input from the user
 subprocess.call('../vendor/bin/premake5 vs2019') 
