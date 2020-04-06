d00c69e0c718c414c0a6bf249e273c1c5f2f5bfa
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-06 17:30:25 -0400

    done with BASIC framework, this is just to get up and running

--- efgl/efgl/src/efgl.h
+++ efgl/efgl/src/efgl.h
@@ -2,3 +2,8 @@
 #include "efpch.h"
 
 #include "Renderer.h"
+#include "Shader.h"
+#include "object/IndexBuffer.h"
+#include "object/VertexArray.h"
+#include "object/VertexBuffer.h"
+#include "object/VertexBufferLayout.h"
