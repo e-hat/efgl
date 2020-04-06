5187780efbcac2a566e98182e7e548c535a4969b
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-06 11:00:30 -0400

    now have script to make the executable and clean up folders

---
+++ setup/make_exec.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.call('pyinstaller -F setup.py')
+subprocess.call('mv -f dist/setup.exe .')
+subprocess.call('rm -rf dist')
+subprocess.call('rm -rf build')
+subprocess.call('rm -rf __pycache__/')
+subprocess.call('rm -f setup.spec')
