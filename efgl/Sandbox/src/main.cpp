15026a6b32b7b5fb695024cd528a77ee32224485
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-07 12:54:44 -0400

    now have sandbox program with example shader to draw triangle

--- efgl/Sandbox/src/main.cpp
+++ efgl/Sandbox/src/main.cpp
@@ -1,10 +1,67 @@
 #include <efgl.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
 
 #include <iostream>
 
 using namespace std;
+using namespace efgl;
 
 int main() {
-	cout << "Hello World" << endl;
 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
+	};
+
+	efgl::Shader shader("src/shaders/shader.glsl");
+	shader.bind();
+
+	efgl::ogl::VertexBuffer vb(vertices, sizeof(vertices));
+
+	efgl::ogl::VertexBufferLayout vbl;
+	vbl.push<float>(3, false);
+	efgl::ogl::VertexArray vao;
+	vao.addBuffer(vb, vbl);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.bind();
+		vao.bind();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 1;
 }
---
+++ efgl/Sandbox/src/shaders/shader.glsl
@@ -0,0 +1,17 @@
+#shader vertex
+#version 460 core
+
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+#shader fragment
+#version 460 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+} 
