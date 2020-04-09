6cc057359f072961b35dc557fb7241b31f64c517
deadbird11 <eddiejr@hatfieldfamily.com>
2020-04-09 16:48:15 -0400

    updated code here to reflect newest improvements to API

--- efgl/Sandbox/src/main.cpp
+++ efgl/Sandbox/src/main.cpp
@@ -11,28 +11,37 @@ using namespace efgl::ogl;
 
 int main() {
 
-	GLwindow* window = GLwindow::getWindow();
-	window->init(800, 600, "Hello window");
+	GLwindow* window = GLwindow::init(800, 600, "Hello window");
 
 	float vertices[] = {
-	 0.0f,  0.5f, 0.0f, // top
-	 0.5f, -0.5f, 0.0f, // bottom right
-	-0.5f, -0.5f, 0.0f  // bottom left
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
 	};
 
 	unsigned int indices[] = {
-		0, 1, 2
+		0, 1, 2,
+		3, 0, 2
 	};
 
-	IndexBuffer ib(indices, 3);
+	Texture2D texture1("src/resources/container.jpg", "container");
+	Texture2D texture2("src/resources/awesomeface.png", "face", true);
+
+	IndexBuffer ib(indices, 6);
 
 	Shader shader("src/shaders/shader.glsl");
 	shader.bind();
+	shader.setUniform("texture1", 0);
+	shader.setUniform("texture2", 1);
 
 	VertexBuffer vb(vertices, sizeof(vertices));
 
 	VertexBufferLayout vbl;
 	vbl.push<float>(3, false);
+	vbl.push<float>(3, false);
+	vbl.push<float>(2, false);
 	VertexArray vao;
 	vao.addBuffer(vb, vbl);
 
@@ -43,9 +52,12 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		texture1.bind(0);
+		texture2.bind(1);
+
 		shader.bind();
 		vao.bind();
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		window->swap();
 	}
--- efgl/Sandbox/src/shaders/shader.glsl
+++ efgl/Sandbox/src/shaders/shader.glsl
@@ -2,20 +2,30 @@
 #version 460 core
 
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
-out vec4 position;
+out vec3 ourColor;
+out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	position = gl_Position;
+    gl_Position = vec4(aPos.x, aPos.y, 
+					   aPos.z, 1.0);
+	ourColor = aColor;
+	TexCoord = aTexCoord;
 }
 #shader fragment
 #version 460 core
-in vec4 position;
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = position;
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 } 
