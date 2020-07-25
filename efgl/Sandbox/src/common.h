66994207367aa64ec34675ecd5ffd72b486d6458
deadbird11 <eddiejr@hatfieldfamily.com>
2020-07-25 10:11:23 -0400

    keeping common includes here

---
+++ efgl/Sandbox/src/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <efgl.h>
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <cmath>
+#include <iostream>
