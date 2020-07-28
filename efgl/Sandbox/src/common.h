47fc4aafbd5730a9417d041b1262ba25e292117f
deadbird11 <eddiejr@hatfieldfamily.com>
2020-07-27 22:59:24 -0400

    fixed up common header

--- efgl/Sandbox/src/common.h
+++ efgl/Sandbox/src/common.h
@@ -13,3 +13,16 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+
+using namespace efgl;
+using namespace efgl::ogl;
+
+static const int SCREEN_WIDTH = 800;
+static const int SCREEN_HEIGHT = 800;
+
+static bool firstMouse = true;
+static float lastX = SCREEN_WIDTH / 2.0f;
+static float lastY = SCREEN_HEIGHT / 2.0f;
+static float deltaTime = 0.0f;
+static float lastFrame = 0.0f;
+
