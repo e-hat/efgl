8ef4fb4074faa73548b0c5afe4dbd2907d3c4b51
deadbird11 <eddiejr@hatfieldfamily.com>
2020-08-03 20:23:27 -0400

    saved EmissionMap example

---
+++ efgl/Sandbox/src/examples/EmissionMap/EmissionMap.cpp
@@ -0,0 +1,219 @@
+#include "EmissionMap.h"
+#include "common.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window, float deltaTime);
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+int EmissionMap() {
+	GLwindow* window = GLwindow::init(SCREEN_WIDTH, SCREEN_HEIGHT, "Colors");
+
+	glfwMakeContextCurrent(window->getWindow());
+	glfwSetFramebufferSizeCallback(window->getWindow(), framebuffer_size_callback);
+	glfwSetCursorPosCallback(window->getWindow(), mouse_callback);
+	glfwSetScrollCallback(window->getWindow(), scroll_callback);
+
+	// tell GLFW to capture our mouse
+	//glfwSetInputMode(window->getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	float vertices[] = {
+		// positions          // normals           // texture coords
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+	};
+
+	VertexBuffer vbo(vertices, sizeof(vertices));
+
+	VertexArray vao;
+	VertexBufferLayout vbl;
+	vbl.push<float>(3);
+	vbl.push<float>(3);
+	vbl.push<float>(2);
+	vao.addBuffer(vbo, vbl);
+
+	VertexArray lightVAO;
+	VertexBufferLayout lightVBL;
+	lightVBL.push<float>(3);
+	lightVBL.push<float>(3);
+	lightVBL.push<float>(2);
+	lightVAO.addBuffer(vbo, lightVBL);
+
+	Shader objectShader("src/shaders/object_shader.glsl");
+	Shader lightShader("src/shaders/light_shader.glsl");
+
+	Texture2D diffuseMap = TextureManager::loadTexture("src/resources/container2.png", "diffuseMap");
+	Texture2D specularMap = TextureManager::loadTexture("src/resources/container2_specular.png", "specularMap");
+	Texture2D emissionMap = TextureManager::loadTexture("src/resources/emission.jpg", "emissionMap");
+	
+	glEnable(GL_DEPTH_TEST);
+
+	glm::vec3 lightPos(0.5f, 0.5f, 1.0f);
+
+	// render loop
+	// ----------------
+	while (!window->shouldClose()) {
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window->getWindow(), deltaTime);
+
+		// set background
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -2.0f));
+
+		objectShader.bind();
+		objectShader.setUniform("model", model);
+		objectShader.setUniform("view", view);
+		objectShader.setUniform("proj", proj);
+
+		//objectShader.setUniform("material.ambient", glm::vec3(1.0f, 0.5f, 0.31f));
+		//objectShader.setUniform("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
+		diffuseMap.bind(0);
+		objectShader.setUniform("material.diffuse", 0);
+		specularMap.bind(1);
+		objectShader.setUniform("material.specular", 1);
+		emissionMap.bind(2);
+		objectShader.setUniform("material.emission", 2);
+
+		objectShader.setUniform("material.shininess", 32.0f);
+
+		objectShader.setUniform("light.ambient", 0.2f, 0.2f, 0.2f);
+		objectShader.setUniform("light.diffuse", 0.5f, 0.5f, 0.5f); // darken diffuse light a bit
+		objectShader.setUniform("light.specular", 1.0f, 1.0f, 1.0f);
+
+		/*objectShader.setUniform("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
+		objectShader.setUniform("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));*/
+		objectShader.setUniform("lightPos", lightPos);
+		objectShader.setUniform("viewPos", camera.Position);
+
+		vao.bind();
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		objectShader.unbind();
+		vao.unbind();
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f));
+
+		lightShader.bind();
+		lightShader.setUniform("model", model);
+		lightShader.setUniform("view", view);
+		lightShader.setUniform("proj", proj);
+
+		lightShader.setUniform("specularColor", 1.0f, 1.0f, 1.0f);
+
+		lightVAO.bind();
+		lightShader.bind();
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		lightShader.unbind();
+		lightVAO.unbind();
+
+		window->swap();
+	}
+
+	return 0;
+}
+
+// below is from tutorial https://learnopengl.com/
+
+void processInput(GLFWwindow* window, float deltaTime)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
---
+++ efgl/Sandbox/src/examples/EmissionMap/shaders/object_shader.glsl
@@ -0,0 +1,73 @@
+#shader vertex
+#version 460 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
+out vec3 FragPos;
+out vec3 Normal;
+out vec2 TexCoords;
+
+void main()
+{
+	gl_Position = proj * view * model * vec4(aPos, 1.0);
+	FragPos = vec3(model * vec4(aPos, 1.0));
+	Normal = mat3(transpose(inverse(model))) * aNormal;
+	TexCoords = aTexCoords;
+}
+#shader fragment
+#version 460 core
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+struct Material {
+	sampler2D diffuse;
+	sampler2D specular;
+	sampler2D emission;
+	float shininess;
+};
+
+struct Light {
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+
+uniform Material material;
+uniform Light light;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+out vec4 outputColor;
+
+void main()
+{
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoords)));
+
+	vec3 emission = vec3(texture(material.emission, TexCoords));
+
+	vec3 result = ambient + diffuse + specular + emission;
+	outputColor = vec4(result, 1.0);
+}
