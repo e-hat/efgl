4120b537125cd1350a15b2e12553acd2204a5892
deadbird11 <eddiejr@hatfieldfamily.com>
2020-09-01 20:37:14 -0400

    Added first model render as example

---
+++ efgl/Sandbox/src/examples/FirstModelRender/FirstModelRender.cpp
@@ -0,0 +1,124 @@
+#include "../../common.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window, float deltaTime);
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+int main() {
+	Window* window = Window::Init(SCREEN_WIDTH, SCREEN_HEIGHT, "Models");
+
+	glfwSetFramebufferSizeCallback(window->GetWindow(), framebuffer_size_callback);
+	glfwSetCursorPosCallback(window->GetWindow(), mouse_callback);
+	glfwSetScrollCallback(window->GetWindow(), scroll_callback);
+
+	// tell GLFW to capture our mouse
+	//glfwSetInputMode(window->getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	Instrumentor::Get().BeginSession("Model building");
+	Model backpack("src/examples/FirstModelRender/resources/backpack/backpack.obj");
+	Instrumentor::Get().EndSession();
+	Shader shader("src/examples/FirstModelRender/shaders/shader.glsl");
+
+	glEnable(GL_DEPTH_TEST);
+
+	// render loop
+	// ----------------
+	while (!window->ShouldClose()) {
+		float currentFrame = (float)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window->GetWindow(), deltaTime);
+
+		// set background
+		glClearColor(0.0f, 0.0f, 0.f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		shader.Bind();
+
+		glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+
+		shader.SetUniform("proj", proj);
+		shader.SetUniform("view", view);
+		shader.SetUniform("model", model);
+
+		shader.SetUniform("material.shininess", 32.0f);
+
+		shader.SetUniform("viewPos", camera.Position);
+
+		shader.SetUniform("pointLight.position", 0.5f, 0.5f, 0.5f);
+		shader.SetUniform("pointLight.ambient", 0.3f, 0.15f, 0.15f);
+		shader.SetUniform("pointLight.diffuse", 1.0f, 0.8f, 0.8f);
+		shader.SetUniform("pointLight.specular", glm::vec3(1.0f));
+		shader.SetUniform("pointLight.constant", 1.0f);
+		shader.SetUniform("pointLight.linear", 0.09f);
+		shader.SetUniform("pointLight.quadratic", 0.032f);
+
+		backpack.Draw(shader);
+
+		window->Swap();
+	}
+
+
+	return 0;
+}
+
+// below is from tutorial https://learnopengl.com/
+
+void processInput(GLFWwindow* window, float deltaTime)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(CameraMovement::RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = (float)xpos - lastX;
+	float yoffset = lastY - (float)ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = (float)xpos;
+	lastY = (float)ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
---
+++ efgl/Sandbox/src/examples/FirstModelRender/shaders/shader.glsl
@@ -0,0 +1,84 @@
+#shader vertex
+#version 460 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
+out vec3 FragPos;
+out vec3 Normal;
+out vec2 TexCoords;
+
+void main()
+{
+	gl_Position = proj * view * model * vec4(aPos, 1.0);
+	FragPos = vec3(model * vec4(aPos, 1.0));
+	Normal = mat3(transpose(inverse(model))) * aNormal;
+	TexCoords = aTexCoords;
+}
+#shader fragment
+#version 460 core
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+
+uniform struct {
+	sampler2D texture_diffuse1;
+	sampler2D texture_specular1;
+
+	float shininess;
+} material;
+
+struct PointLight {
+	vec3 position;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+uniform PointLight pointLight;
+
+uniform vec3 viewPos;
+
+out vec4 FragColor;
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+void main() {
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
+
+	FragColor = vec4(CalcPointLight(pointLight, norm, FragPos, viewDir), 1.0);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
+	vec3 lightDir = normalize(light.position - fragPos);
+
+	float diff = max(dot(normal, lightDir), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+	float distance = length(light.position - fragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
+	vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	return ambient + diffuse + specular;
+}
