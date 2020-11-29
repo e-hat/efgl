2ea922ec98255a630d179f2fdc940115bc7846de
deadbird11 <eddiejr@hatfieldfamily.com>
2020-11-29 17:23:57 -0500

    added TODO comment

--- efgl/src/material/IMaterial.h
+++ efgl/src/material/IMaterial.h
@@ -4,7 +4,9 @@
 #include "Shader.h"
 
 namespace efgl {
-	
+	// TODO: maybe I need to link materials and shaders somehow
+	// as, in many cases, a material is meant for one specific shader
+	// this would require some rethinking of Scene
 	class IMaterial {
 	public:
 		virtual void SetShaderUniforms(Shader& shader) = 0;
