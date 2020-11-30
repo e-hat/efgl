62060d1840aef85cd34e0fb863ff537a73bf1c7c
deadbird11 <eddiejr@hatfieldfamily.com>
2020-11-29 19:55:30 -0500

    stowed subdivision demo

--- Sandbox/src/common.h
+++ Sandbox/src/common.h
@@ -3,24 +3,6 @@
 #include "scene/Camera.h"
 #include "Window.h"
 #include "geometry/Model.h"
-#include "Shader.h"
-#include "util/Profile.h"
-#include "geometry/prim/Quad.h"
-#include "scene/Light.h"
 #include "Application.h"
-#include "texture/Texture.h"
-
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/string_cast.hpp>
-#include <glm/gtx/quaternion.hpp>
-
-#include <string>
-#include <cmath>
-#include <iostream>
-#include <vector>
-#include <functional>
 
 
