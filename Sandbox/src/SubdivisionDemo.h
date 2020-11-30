7a6545c5bbbbe2b59c6bbae4c60e370c8146fcb1
deadbird11 <eddiejr@hatfieldfamily.com>
2020-11-30 14:03:06 -0500

    cleaned up input management & deltaTime

--- Sandbox/src/SubdivisionDemo.h
+++ Sandbox/src/SubdivisionDemo.h
@@ -4,19 +4,13 @@
 #include "geometry/Model.h"
 #include "Shader.h"
 #include <imgui.h>
+#include "InputManager.h"
+#include "util/Time.h"
 
 using namespace efgl;
 
 static const int SCREEN_WIDTH = 1080;
 static const int SCREEN_HEIGHT = 920;
-
-static bool firstMouse = true;
-static float lastX = SCREEN_WIDTH / 2.0f;
-static float lastY = SCREEN_HEIGHT / 2.0f;
-static float deltaTime = 0.0f;
-static float lastFrame = 0.0f;
-
-static Camera camera;
 
 class SubdivisionDemo : public Application {
 public:
@@ -27,9 +21,6 @@ public:
 	}
 
 	virtual void Init() override {
-		glfwSetFramebufferSizeCallback(window->GetNativeWindow(), framebuffer_size_callback);
-		glfwSetCursorPosCallback(window->GetNativeWindow(), mouse_callback);
-		glfwSetScrollCallback(window->GetNativeWindow(), scroll_callback);
 
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_MULTISAMPLE);
@@ -38,14 +29,13 @@ public:
 		subdivisionShader = MakeRef<Shader>("shaders/subdivision_demo.glsl");
 		camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT, glm::vec3(0.0f, 0.0f, 3.0f));
 		numSlices = 25;
+
+		InputManager::SetGLFWCallbacks(window, &camera);
 	}
 
 	virtual void OnRender() override {
-		float currentFrame = (float)glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
 
-		processInput(window->GetNativeWindow(), deltaTime);
+		InputManager::ProcessInput(window, Time::GetDeltaTime());
 
 		// set background
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
@@ -86,61 +76,8 @@ public:
 private:
 	Ref<Model> sponza;
 	Ref<Shader> subdivisionShader;
+	Camera camera;
 
 	int numSlices;
-
-	// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-	static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-	{
-		// make sure the viewport matches the new window dimensions; note that width and 
-		// height will be significantly larger than specified on retina displays.
-		glViewport(0, 0, width, height);
-	}
-
-
-	// glfw: whenever the mouse moves, this callback is called
-	// -------------------------------------------------------
-	static void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-	{
-		if (firstMouse)
-		{
-			lastX = (float)xpos;
-			lastY = (float)ypos;
-			firstMouse = false;
-		}
-
-		float xoffset = (float)xpos - lastX;
-		float yoffset = lastY - (float)ypos; // reversed since y-coordinates go from bottom to top
-
-		lastX = (float)xpos;
-		lastY = (float)ypos;
-
-		camera.ProcessMouseMovement(xoffset, yoffset);
-	}
-
-	// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-	// ----------------------------------------------------------------------
-	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-	{
-		camera.ProcessMouseScroll(yoffset);
-	}
-
-	// below is from tutorial https://learnopengl.com/
-
-	static void processInput(GLFWwindow* window, float deltaTime)
-	{
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-			glfwSetWindowShouldClose(window, true);
-
-		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-			camera.ProcessKeyboard(CameraMovement::FORWARD, deltaTime);
-		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-			camera.ProcessKeyboard(CameraMovement::BACKWARD, deltaTime);
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-			camera.ProcessKeyboard(CameraMovement::LEFT, deltaTime);
-		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-			camera.ProcessKeyboard(CameraMovement::RIGHT, deltaTime);
-	}
 
 };
