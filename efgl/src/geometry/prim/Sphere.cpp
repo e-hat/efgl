baee168a0a8b39d6eeb896718eaf72fe3b46cc70
e-hat <edward.hatfield@tufts.edu>
2021-01-09 16:56:34 -0500

    added Sphere

--- efgl/src/geometry/prim/Sphere.cpp
+++ efgl/src/geometry/prim/Sphere.cpp
@@ -4,37 +4,59 @@
 
 using namespace std;
 
+static const float R = 1.0f;
+
 namespace efgl
 {
-	Sphere::Sphere(Ref<IMaterial> pMat, unsigned int lod)
+	Sphere::Sphere(Ref<IMaterial> pMat, unsigned int vSegments, unsigned int hSegments)
 	{
 		pMaterial = pMat;
 
-		if (m_sCachedGeometry.empty()) m_sCachedGeometry = genIcoSphereVertices(lod);
-		Vertices = m_sCachedGeometry;
+		float vStep = glm::pi<float>() / (float)vSegments;
+		float hStep = 2 * glm::pi<float>() / (float)hSegments;
+
+		for (int v = 0; v <= vSegments; ++v)
+		{
+			for (int h = 0; h <= hSegments; ++h)
+			{
+				float phi = glm::pi<float>() / 2.0f - v * vStep;
+				float theta = h * hStep;
+
+				float x = R * glm::cos(phi) * glm::cos(theta);
+				float y = R * glm::cos(phi) * glm::sin(theta);
+				float z = R * glm::sin(phi);
+
+				Vertices.push_back(Vertex{
+					glm::vec3(x, y, z),
+					glm::vec3(x, y, z),
+					glm::vec2((float)h / (float)hSegments, (float)v / (float)vSegments)
+				});
+			}
+		}
+
+		int k1, k2;
+		for (int i = 0; i < vSegments; ++i)
+		{
+			k1 = i * (hSegments + 1);
+			k2 = k1 + hSegments + 1;
+
+			for (int j = 0; j < hSegments; ++j, ++k1, ++k2)
+			{
+				if (i != 0)
+				{
+					Indices.push_back(k1);
+					Indices.push_back(k2);
+					Indices.push_back(k1 + 1);
+				}
+
+				if (i != vSegments - 1)
+				{
+					Indices.push_back(k1 + 1);
+					Indices.push_back(k2);
+					Indices.push_back(k2 + 1);
+				}
+			}
+		}
+
 	}
-
-	// translated from JavaScript example here: 
-	vector<Vertex> Sphere::genIcoSphereVertices(unsigned int lod)
-	{
-		const auto f = (1.0f + glm::sqrt(5.0f)) / 2.0f;
-		const auto T = glm::pow(4, lod);
-
-		// 20 triangle icosahedron
-		const float verts[] = {
-			-1, f, 0, 1, f, 0, -1, -f, 0, 1, -f, 0,
-			0, -1, f, 0, 1, f, 0, -1, -f, 0, 1, -f,
-			f, 0, -1, f, 0, 1, -f, 0, -1, -f, 0, 1
-		};
-
-		const unsigned int triIndices[] = {
-			0, 11, 5, 0, 5, 1, 0, 1, 7, 0, 7, 10, 0, 10, 11,
-			11, 10, 2, 5, 11, 4, 1, 5, 9, 7, 1, 8, 10, 7, 6,
-			3, 9, 4, 3, 4, 2, 3, 2, 6, 3, 6, 8, 3, 8, 9,
-			9, 8, 1, 4, 9, 5, 2, 4, 11, 6, 2, 10, 8, 6, 7
-		};
-
-
-	}
-
 }
--- efgl/src/geometry/prim/Sphere.h
+++ efgl/src/geometry/prim/Sphere.h
@@ -3,19 +3,13 @@
 
 #include "geometry/Mesh.h"
 
+#include <map>
+
 namespace efgl 
 {
 	class Sphere : public Mesh 
 	{
 	public:
-		Sphere(Ref<IMaterial> pMat, unsigned int lod);
-	private:
-		// IcoSphere to minimize # vertices
-		std::vector<Vertex> genIcoSphereVertices(unsigned int lod);
-		Vertex makeVertex(const glm::vec3& pt);
-		int getMiddlePoint(int p1, int p2);
-
-		// no need to recreate sphere geometry every time
-		static std::vector<Vertex> m_sCachedGeometry;
+		Sphere(Ref<IMaterial> pMat, unsigned int vSegments, unsigned int hSegments);
 	};
 }
