d31b83011f69cc6e756bbe9587535e447bc6e27e
e-hat <edward.hatfield@tufts.edu>
2021-07-03 10:09:42 -0600

    Add PbrMaterial class

---
+++ efgl/src/material/PbrMaterial.cpp
@@ -0,0 +1,36 @@
+#include "efpch.h"
+
+#include "PbrMaterial.h"
+
+#include <../tracy/Tracy.hpp>
+#include <../tracy/TracyOpenGL.hpp>
+
+namespace efgl {
+	void PbrMaterial::SetShaderUniforms(Shader& shader) 
+	{
+		ZoneScopedC(tracy::Color::Orchid);
+		TracyGpuZoneC("PbrMaterial::SetShaderUniforms", tracy::Color::Sienna);
+
+		shader.Bind();
+
+		for (int i = 0; i < Albedos.size(); ++i) {
+			shader.SetUniform("material.texture_diffuse" + std::to_string(i + 1), i);
+
+			Albedos[i]->Bind(i);
+		}
+
+		for (int i = 0; i < Roughnesses.size(); ++i) {
+			int slot = i + Albedos.size();
+			shader.SetUniform("material.texture_specular" + std::to_string(i + 1), slot);
+
+			Roughnesses[i]->Bind(slot);
+		}
+
+		for (int i = 0; i < Metalnesses.size(); ++i) {
+			int slot = i + Albedos.size() + Roughnesses.size();
+			shader.SetUniform("material.texture_metalness" + std::to_string(i + 1), slot);
+
+			Metalnesses[i]->Bind(slot);
+		}
+	}
+}
