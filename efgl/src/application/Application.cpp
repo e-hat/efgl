80bb279772b71a4e70283051f8d68ca3ebdd3ddc
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-11-12 18:10:00 -0500

    Update ImGui to use latest docking branch

--- efgl/src/application/Application.cpp
+++ efgl/src/application/Application.cpp
@@ -6,13 +6,11 @@
 #include <../tracy/TracyOpenGL.hpp>
 
 #ifdef DEBUG
-#include "imgui.h"
-#include "examples/imgui_impl_glfw.h"
-#include "examples/imgui_impl_opengl3.h"
-
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "examples/imgui_impl_opengl3.cpp"
-#include "examples/imgui_impl_glfw.cpp"
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
 #endif
 
 namespace efgl 
