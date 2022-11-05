fc36ec8e5595378ebbbfcb078581eb03caed9b36
Edward Hatfield <edward.hatfield@tufts.edu>
2022-11-05 17:50:43 -0400

    Add vulkan libraries to CMake build
    Also get rid of Glad, don't need it no more! :D

--- efgl/src/efpch.cpp
+++ efgl/src/efpch.cpp
@@ -1,24 +1,6 @@
 #include "efpch.h"
 
-#include <glad/glad.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
-
-namespace efgl
-{
-	void GLClearError()
-	{
-		while (glGetError() != GL_NO_ERROR);
-	}
-
-	bool GLLogCall(const char* function, const char* file, int line)
-	{
-		while (GLenum error = glGetError())
-		{
-			std::cout << "[OpenGL Error] (" << error << "): " << function <<
-				" " << file << ":" << line << std::endl;
-			return false;
-		}
-		return true;
-	}
-}
